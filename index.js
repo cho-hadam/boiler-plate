312b3da91e6b0bf7154005152c2c3f7dd8e6a0bf
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-08 01:39:18 +0900

    BodyParser & PostMan & Sign Up

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express();
 // back server port
 const port = 5000;
 
+const bodyParser = require("body-parser");
+// User model
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
+
 // MongoDB Connect
 const mongoose = require("mongoose");
 mongoose
@@ -22,6 +31,18 @@ mongoose
 
 // root directory -> print Hello World
 app.get("/", (req, res) => res.send("Hello World!"));
+
+// get data from client and insert to database
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 // run -> print console
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
